5f41a77cff2f65707297001a838e00fb709ef476
akinmaurice <akinmaurice@gmail.com>
2018-01-23 17:41:49 +0100

    error handling for invalid routes

--- app.js
+++ app.js
@@ -26,5 +26,25 @@ app.use('/graphql', graphqlHTTP(() => ({
   pretty: true,
 })));
 
+
+// Error Handlers
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ status: 401, error: 'Invalid Endpoint' });
+});
+
 // done! we export it so we can start the site in start.js
 module.exports = app;
